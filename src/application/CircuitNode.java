2d620c07eb33d8fa1cc7f6ba438c775f882053a6
太郎 <@com>
2018-12-16 21:25:16 +0900

    だれかてつだって

--- src/application/CircuitNode.java
+++ src/application/CircuitNode.java
@@ -4,35 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CircuitNode {//接点クラス
-	private Integer ID=null;
-	private double current=0;//a
-	private double voltage=0;
-	private List<Element> elements=new ArrayList<Element>();
+	private Integer ID=null;//節点のID
+	private double current=0;//節点電流
+	private double voltage=0;//節点電圧
+	private List<Element> elements=new ArrayList<Element>();//節点につながっている素子のIDのリスト
 
 	//http://www.ecircuitcenter.com/SpiceTopics/Overview/Overview.htm
 	//http://ednjapan.com/edn/articles/1306/04/news005.html
 
 	CircuitNode(){
-		System.out.println("gittest");//
 	}
-
+	
+	//節点に素子をつなげる
 	public void add(Element element) {
-		elements.add(element);
+		elements.add(element.getID(), element);
 	}
 
+	//節点につながっている素子を返す
 	public Element get(int num) {
 		return elements.get(num);
 	}
-
-	public double getNodalCurrent() {//TODO 接点に印加される電流を計算
+	
+	//節点から素子を取り除く
+	public void remove(Integer ID) {
+		elements.remove(ID.intValue());
+	}
+	
+	//TODO 接点に印加される電流を計算
+	public double getNodalCurrent() {
 		return 0;
 	}
-
-	public double getNodalVoltage() {//TODO 接点に印加される電圧を計算
+	
+	//TODO 接点に印加される電圧を計算
+	public double getNodalVoltage() {
 		return 0;
 	}
-
-//	public double getNodalImpedance() {//TODO 接点と接点の間の抵抗値を計算(Circuitクラスでやったほうがいいかも)
+	
+	//TODO 接点と接点の間の抵抗値を計算(Circuitクラスでやったほうがいいかも)
+//	public double getNodalImpedance() {
 //		//return Matrix2D.mult(new Matrix2D(), b);
 //		return 0;
 //	}
@@ -60,8 +69,5 @@ public class CircuitNode {//接点クラス
 	public void setVoltage(double voltage) {
 		this.voltage = voltage;
 	}
-
-
-
 
 }
--- src/application/Complex.java
+++ src/application/Complex.java
@@ -1,19 +1,44 @@
 package application;
 
 public class Complex {
-	private Double realPart=null;
-	private Double imaginaryPart=null;
+	private double realPart;//実部
+	private double imaginaryPart;//虚部
+	
+	Complex(double realPart, double imaginary){
+	}
 
+	//実部を返す
 	public Double getRealPart() {
 		return realPart;
 	}
+	
+	//実部を設定
 	public void setRealPart(Double realPart) {
 		this.realPart = realPart;
 	}
+	
+	//虚部を返す
 	public Double getImaginaryPart() {
 		return imaginaryPart;
 	}
+	
+	//虚部を設定
 	public void setImaginaryPart(Double imaginaryPart) {
 		this.imaginaryPart = imaginaryPart;
+	}	
+	
+	//絶対値を返す
+	public double getAbsolute() {
+		return Math.sqrt(realPart*realPart + imaginaryPart*imaginaryPart);
+	}	
+	
+	//偏角を返す
+	public double getArgument() {
+		return Math.atan(imaginaryPart/realPart);
+	}
+	
+	//極座標表示を返す
+	public String getPolar() {
+		return String.valueOf(getAbsolute())+"∠"+String.valueOf(getArgument());
 	}
 }
