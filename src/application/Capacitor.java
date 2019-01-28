80fdea20b2925f6ce1fd25940c9f9048f93bd0dc
過渡現象 <41720721+kogaku6@users.noreply.github.com>
2019-01-28 14:29:45 +0900

    a

---
+++ src/application/Capacitor.java
@@ -0,0 +1,18 @@
+public static double f1(double x, double t){
+     return ;
+}
+
+public static void Deff(double a double b double c) {
+     double x = 2;
+     double frec = 10.0;
+     double t;
+ 
+  for (int t=0 ; t<52 ; t++){
+       double h = Math.pow(2,-t);
+       double diff[t] = (f1(t+h,frec) - f1(t))/h;
+       System.out.println("t("+t+") : h("+h+") : diff = " +diff+"); //計算テスト用に答え表示
+  }
+  
+  //キャパシタの計算(微分)
+  
+}
